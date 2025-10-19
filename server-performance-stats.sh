52b9ac933921901c57587d037e164f3cb2391922
TiPO_MeOW <gips8790@gmail.com>
2025-10-19 23:45:08 +0200

    Improve output readability

--- server-performance-stats.sh
+++ server-performance-stats.sh
@@ -1,12 +1,24 @@
+<<<<<<< HEAD
+=======
+printf "\n\n"
+>>>>>>> e97280c (Improve output readability)
 
 cpu_usage=$(top -bn1 | awk '/%Cpu/ {print 100 - $8}')
 echo "CPU usage: ${cpu_usage}%"
 
+<<<<<<< HEAD
+=======
+printf "\n---\n\n"
+
+>>>>>>> e97280c (Improve output readability)
 free_mem=$(free -m | awk '/Mem:/ {print $4}')
 used_mem=$(free -m | awk '/Mem:/ {print $3}')
 total_mem=$(free -m | awk '/Mem:/ {print $2}')
 echo "Free memory(in mebibytes): ${free_mem}MiB    $(( ($free_mem*100)/$total_mem ))% From total memory"
 echo "Used memory(in mebibytes): ${used_mem}MiB    $(( ($used_mem*100)/$total_mem ))% From total memory"
+
+printf "\n---\n\n"
+
 total_disk=$(df -h | grep "/" -w | awk '{print $2}')
 used_disk=$(df -h | grep "/" -w | awk '{print $3}')
 free_disk=$(df -h | grep "/" -w | awk '{print $4}')
@@ -15,3 +27,5 @@ used_disk_percentage=${used_disk_percentage%?}
 echo "Total disk size: ${total_disk}     Used:${used_disk_percentage}%    Free:$((100-${used_disk_percentage}))%"
 echo "Used space: $used_disk"
 echo "Free space: $free_disk"
+
+printf "\n\n"
